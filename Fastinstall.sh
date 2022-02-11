d2f12b3b27d5aeb4190125129c7392ee0504970d
LuciferMu <97560142+LuciferMu@users.noreply.github.com>
2022-02-12 00:46:42 +0300

    Update Fastinstall.sh

--- Fastinstall.sh
+++ Fastinstall.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-cd $HOME/BLACKBOTSS
+cd $HOME/luciferMu
 rm -rf $HOME/.telegram-cli
 install() {
 apt install dnsutils
 rm -rf $HOME/.telegram-cli
 sudo chmod +x tg
-chmod +x BLACKBOTSS
+chmod +x luciferMu
 chmod +x ts
 ./ts
 }
